4fbc37a178481c1eaed0ea92bd933a71a51026b2
shivangrawat <sr6364@nyu.edu>
2024-06-23 00:11:03 -0400

    added new algorithm

--- spectre/spectrum_general/recursive_spectrum_g.py
+++ spectre/spectrum_general/recursive_spectrum_g.py
@@ -4,13 +4,14 @@ import numpy as np
 import torch
 from torch.func import jacrev
 import sympy as sp
+from sympy import Rational
 import os
 import timeit
 from functools import lru_cache
 
 
 class recursive_solution_g:
-    def __init__(self, G=None, L=None, Y=None):
+    def __init__(self, G=None, Y=None):
         """
         In this constructor function, we define and assign the different matrices "O"
         and list "l", upon which our spectrum solution depends.
@@ -19,14 +20,13 @@ class recursive_solution_g:
         :param S: the rate matrix
         """
         self.G = sp.Matrix([[sp.Rational(str(j)) for j in i] for i in G.tolist()])
-        self.L = sp.Matrix([[sp.Rational(str(j)) for j in i] for i in L.tolist()])
-        self.S = sp.Matrix([[sp.Rational(str(j)) for j in i] for i in Y.tolist()])
-        self.Y = self.L * self.S * self.L.T
+        self.Y = sp.Matrix([[sp.Rational(str(j)) for j in i] for i in Y.tolist()])
         self.Y_inv = self.Y.inv()
         self.n = G.shape[0]
 
         """Define the solution lists"""
         self.P = [sp.zeros(self.n, self.n) for _ in range(2 * self.n + 1)]
+        self.S = [sp.zeros(self.n, self.n) for _ in range(2 * self.n + 1)]
         self.q = [sp.Rational("1") for _ in range(2 * self.n + 1)]
 
         """Find the solution matrices"""
@@ -42,10 +42,13 @@ class recursive_solution_g:
             self.P[alpha - 1] = recursive_solution_g.P(
                 self.q[alpha + 1], self.Y, self.G, self.P[alpha], self.P[alpha+1]
             )
+            self.S[2 * self.n - 1 - alpha] = self.S_eqn(alpha, self.P[alpha - 1])
             self.q[alpha] = recursive_solution_g.q(
                 self.G, self.n, alpha, self.Y_inv, self.P[alpha], self.P[alpha-1]
             )
+        self.S[2 * self.n - 1] = self.S_eqn(0, self.P[-1])
         self.q[0] = recursive_solution_g.q(self.G, self.n, 0, self.Y_inv, self.P[0], self.P[-1])
+        self.S[2 * self.n] = self.S_eqn(-1, self.P[-1])  
         return None
 
     @staticmethod
@@ -62,7 +65,18 @@ class recursive_solution_g:
         This function returns q_{alpha} using the recursive solution.
         q_{alpha} = (Tr(Y^-1 * G * P_alpha * G^T) - Tr(Y^-1 * G * P_{alpha - 1} )) / (n-alpha/2)
         """
-        return ((Y_inv * G * P * G.T).trace() - (Y_inv * G * P1).trace()) / (n - alpha/2)
+        return ((Y_inv * G * P * G.T).trace() - (Y_inv * G * P1).trace()) / (n - Rational(alpha, 2))
+
+    def S_eqn(self, alpha, P):
+        """
+        This function returns S_{2n-1-alpha} using the recursive solution.
+        S_{2n-1-alpha} = P_{alpha-1} - \sum_{k=0}^{2n-2-alpha} q_{alpha+k+1} S_{k}
+        """
+        # write the function
+        S = P
+        for k in range(2 * self.n - 1 - alpha):
+            S -= self.q[alpha + k + 1] * self.S[k]
+        return S
 
     def q_all_coeffs(self):
         """
